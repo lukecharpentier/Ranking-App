fb21ae66da108ad7adc4ed2ee6ca65176a5b4062
gibbshooper <gibbshooper333@gmail.com>
2019-03-14 08:07:05 -0500

    About to Merge

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var myrankingsRouter = require('./routes/myrankings');
+var aboutRouter = require('./routes/about');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/myrankings', myrankingsRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
